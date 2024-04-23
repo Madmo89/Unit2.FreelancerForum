737a52fccc6f193fb3cf6ec346e6202ab2aedb8a
Madison Moulder <madmogalaxy89@att.net>
2024-04-23 14:56:33 -0400

    forum

---
+++ forum.js
@@ -0,0 +1,42 @@
+const ele =document.createElement("div");
+
+
+
+document.getElementById("title").innerHTML="Freelancer Forum"
+
+document.getElementById("start").innerHTML="The Average starting price is"
+    
+
+
+document.getElementById("head").innerHTML="Available Freelancers"
+
+document.getElementById("name").innerHTML="Name   Occupation  Starting Price";
+
+ele.innerHTML=" Alice   Writer  $30";
+
+
+
+
+
+
+
+
+
+
+
+document.body.appendChild(ele);
+
+
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
